e493a9e54eb7239b42ee2f2c1e9e602e12068485
aestears <astears@uwyo.edu>
2020-11-28 21:29:39 -0700

    Create R script to add model results to overleaf document

---
+++ OverleafModelResults.R
@@ -0,0 +1,11 @@
+# /////////////////////////////////////////////////
+# Script for including model output in Overleaf
+# A. Stears
+# 28 November 2020
+# /////////////////////////////////////////////////
+
+## ---- myrcode1
+# load model results
+setwd("/Users/Alice/Dropbox/Grad School/Research/Trait Project/CO_sgs Analysis")
+#get model result data into the environment
+load("./SCRIPTS/models_11232020.RData") 
