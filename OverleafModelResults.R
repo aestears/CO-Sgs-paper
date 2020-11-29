6e0c141562905d2c22f027ef9768a0d737bd7951
aestears <astears@uwyo.edu>
2020-11-28 21:37:53 -0700

    get model result data file and add to repo

--- OverleafModelResults.R
+++ OverleafModelResults.R
@@ -6,6 +6,5 @@
 
 ## ---- myrcode1
 # load model results
-setwd("/Users/Alice/Dropbox/Grad School/Research/Trait Project/CO_sgs Analysis")
 #get model result data into the environment
-load("./SCRIPTS/models_11232020.RData") 
+load("models_11232020.RData") 
