d1625fea0653cba3019fc992229064c0cbb09c8c
Alice Stears <astears@uwyo.edu>
2021-03-09 11:21:36 -0700

    try different precip models

--- scripts/4_Models.R
+++ scripts/4_Models.R
@@ -17,7 +17,7 @@ require(stargazer) #v5.2.2
 require(glmmTMB) #v1.0.2.1
 require(sjPlot) #v2.8.7
 require(effects) #v4.2-0
-require(Ryacas) #v1.1.3.1
+require(ggeffects) #v1.1.3.1
 
 ## clear workspace ##
 rm(list=ls())
@@ -31,7 +31,7 @@ load("./scripts/script3_output.RData")
 
 #### ensure that the structure of the variables is correct ####
 CO_poly_all <- CO_poly_all %>% 
-  dplyr::select(species, quad, year_t, area_t, survives_tplus1, nearEdge_t, area_tplus1, neighbors_5_s, neighbors_10_s, neighbors_15_s, neighbors_20_s, area_s, RTD_s, RDMC_s, SLA_s, SPEI_s, LDMC_s, TLP_s, SRL_s, RDiam_s, SPEI_uniform_s, SPEI_uniform, SPEI_unique, precip_s, logDiffArea) %>% 
+  dplyr::select(species, quad, year_t, area_t, survives_tplus1, nearEdge_t, area_tplus1, neighbors_5_s, neighbors_10_s, neighbors_15_s, neighbors_20_s, area_s, RTD_s, RDMC_s, SLA_s, SPEI_s, LDMC_s, TLP_s, SRL_s, RDiam_s, SPEI_uniform_s, SPEI_uniform, SPEI_unique, precip_s, logDiffArea, area_tplus1_s) %>% 
   mutate(species = as.factor(species), quad = as.factor(quad), year_t = as.factor(year_t), nearEdge_t = as.factor(nearEdge_t))
 
 CO_point_all <- CO_point_all %>% dplyr::select(species, quad, year, survives, nearEdge,  neighbors_10_s, neighbors_15_s, neighbors_20_s, RTD_s, RDMC_s, SLA_s, SPEI_s, LDMC_s, TLP_s, SRL_s, RDiam_s, SPEI_uniform_s, SPEI_uniform, SPEI_unique, precip_s) %>% 
@@ -344,6 +344,29 @@ CO_grow_RDiam<-
   filter(is.na(RDiam_s)==FALSE)
 mGrowRDiam  <- lme4::lmer(logDiffArea ~  neighbors_10_s + SPEI_s * RDiam_s + nearEdge_t + (1|species) + (1|quad) + (1|year_t), data = CO_grow_RDiam , control=lmerControl(optimizer="bobyqa"))
 
+
+#### testing different growth response variables ####
+## log-transformed area in year t: area_s
+## log-transformed area in year t+1: area_tplus1_s
+
+#still using logDiffArea as response, but including a random slope for area_t
+mGrow_size <- lme4::lmer(logDiffArea ~ neighbors_10_s + SPEI_s * TLP_s + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_grow_TLP , control=lmerControl(optimizer="bobyqa"))
+
+## using area_t+1 as the response variable
+mSize_TLP <- lme4::lmer(area_tplus1_s ~ area_s + neighbors_10_s + SPEI_s * TLP_s + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_grow_TLP , control=lmerControl(optimizer="bobyqa"))
+
+mSize_LDMC <- lme4::lmer(area_tplus1_s ~ area_s + neighbors_10_s + SPEI_s * LDMC_s + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_grow_TLP , control=lmerControl(optimizer="bobyqa"))
+## models w/out traits 
+mSize_TLP_NO <- lme4::lmer(area_tplus1_s ~ area_s + neighbors_10_s + SPEI_s + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_grow_TLP , control=lmerControl(optimizer="bobyqa"))
+
+mSize_LDMC_NO <- lme4::lmer(area_tplus1_s ~ area_s + neighbors_10_s + SPEI_s  + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_grow_TLP , control=lmerControl(optimizer="bobyqa"))
+
+## see if including traits improves the models
+diff(AIC(mSize_TLP, mSize_TLP_NO)$AIC) #TLP
+diff(AIC(mSize_LDMC, mSize_LDMC_NO)$AIC) #LDMC
+# including the traits makes the models worse :-( :-( :-( )))
+
+
 #### make growth models without traits for comparison ####
 #fit models
 
@@ -406,6 +429,12 @@ sjPlot::tab_model(mGrowTLP, mGrowLDMC, mGrowSLA, mGrowRDMC, mGrowRTD, mGrowSRL, 
 polySpp <- as.character(unique(CO_grams$species))
 pointSpp <- as.character(unique(CO_point_all$species))
 
+#get trait data
+datWD <- "/Users/Alice/Dropbox/Grad School/Research/Trait Project/Data/CO Analysis Data Files" #set working directory for location of flowering time data file
+setwd(datWD)
+CO_traits <- read.csv("./CO_mean_traits.csv", stringsAsFactors = FALSE)
+
+#l
 #get trait correlations for all species used in the models
 traits_modSpp <- CO_traits[CO_traits$species %in% c(polySpp, pointSpp),]
 
@@ -454,7 +483,7 @@ visreg::visreg2d(m2_grams, xvar = "LDMC_s", yvar =  "SPEI_s", scale = "response"
 visreg::visreg2d(m2_SPEI, xvar = "LDMC_s", yvar =  "SPEI_uniform_s", scale = "response", plot.type = "persp")
 
 visreg::visreg2d(mGrowTLP, xvar = "TLP_s", yvar =  "SPEI_s", scale = "response", plot.type = "persp")
-visreg::visreg2d(mGrowTLP_SPEI, xvar = "TLP_s", yvar =  "SPEI_uniform_s", scale = "response", plot.type = "persp")
+visreg::visreg2d(mGrow_size, xvar = "TLP_s", yvar =  "SPEI_s", scale = "response", plot.type = "persp")
 
 #### compare models using SPEI to models using precip ####
 m1_precip <- glmer(survives_tplus1 ~ area_s + neighbors_10_s + precip_s* TLP_s + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_poly_TLP, family = binomial(link = logit), control=glmerControl(optimizer="bobyqa"))
@@ -744,9 +773,179 @@ Precip_Grow <- ggplot(data = PrecipGrowDat) +
 ##Figures showing response of graminoid survival and growth using uniform SPEI
 plot_grid(Precip_Surv, Precip_Grow, ncol = 2, align = "h", axis = "tb", rel_widths = c(.9,1))
 
-#### try a GAMM model for TLP survival ####
-require(gamm4)
-m1<-gamm(re~s(winterpc1,bs="cr")+logfrz,gamma=1.4,random=list(fem=~1), family=poisson, method="REML",data=end)
-glmer(survives_tplus1 ~ area_s + neighbors_10_s + SPEI_uniform_s * TLP_s + nearEdge_t + (area_s|species) + (1|quad) + (1|year_t), data = CO_poly_TLP, family = binomial(link = logit), control=glmerControl(optimizer="bobyqa"))
+##table of model results
+stargazer(m1_grams, m2_grams, mGrowTLP, mGrowLDMC, type = "html")
 
-m1GAMM <- gamm(survives_tplus1 ~ )
+#### Make figure comparing survival to growth for TLP and LDMC of graminoids using area_tplus1 as response variable ####
+#get 2.5 and 97.5 percentiles of the distribution
+meanSPEI<- mean(CO_poly_TLP$SPEI_s)
+sdSPEI <- sd(CO_poly_TLP$SPEI_s)
+#get 97.5 quantile of the distribution
+SPEI_97_5 <- qnorm(.975, meanSPEI, sdSPEI) 
+SPEI_2_5 <- qnorm(.025, meanSPEI, sdSPEI)
+
+spei_vals <- c(SPEI_2_5, SPEI_97_5)
+
+#for TLP
+TLP_vals <- seq(min(CO_poly_TLP$TLP_s, na.rm = TRUE), max(CO_poly_TLP$TLP_s, na.rm = TRUE), length.out = 20)
+TLP_surv_dat <- ggpredict(m1_grams, terms = c("TLP_s[TLP_vals]", "SPEI_s[spei_vals]"), type = "fixed", back.transform = TRUE)
+
+#for LDMC
+LDMC_vals <- seq(min(CO_poly_LDMC$LDMC_s, na.rm = TRUE), max(CO_poly_LDMC$LDMC_s, na.rm = TRUE), length.out = 20)
+LDMC_surv_dat <- ggpredict(m2_grams, terms = c("LDMC_s[LDMC_vals]", "SPEI_s[spei_vals]"), type = "fixed", back.transform = TRUE)
+
+
+#make a data.frame to contain all of the values for each trait
+UniqueSurvDat <- data.frame(trait = c("scaled(Turgor Loss Point) (MPa)"), x = TLP_surv_dat$x, Surv = TLP_surv_dat$predicted, CI_low = TLP_surv_dat$conf.low, CI_high = TLP_surv_dat$conf.high, SPEI = TLP_surv_dat$group)
+
+UniqueSurvDat <- rbind(UniqueSurvDat, data.frame(trait = c("scaled(Leaf Dry Matter Content) (g/g)"), x = LDMC_surv_dat$x, Surv = LDMC_surv_dat$predicted, CI_low = LDMC_surv_dat$conf.low, CI_high = LDMC_surv_dat$conf.high, SPEI = LDMC_surv_dat$group))
+
+#make data for rug plot
+RugDat_surv <-  data.frame(rug = CO_poly_TLP$TLP_s, trait = "scaled(Turgor Loss Point) (MPa)")
+RugDat_surv <- rbind(RugDat_surv, data.frame(rug = CO_poly_LDMC$LDMC_s, trait = "scaled(Leaf Dry Matter Content) (g/g)"))
+
+#make a multipanel figure that shows only the graminoid survival probs for 3 traits
+UniqueSPEI_Surv <- ggplot(data = UniqueSurvDat) +
+  geom_ribbon(aes(x = x, ymin = CI_low, ymax = CI_high, fill = SPEI), alpha = 0.3) +
+  geom_line(aes(x=x, y = Surv, col = SPEI))  + 
+  geom_rug(aes(x = rug), data = RugDat_surv) +
+  labs(title = "Unique SPEI Graminoid Survival") +
+  xlab(NULL) +
+  ylab("Probability of Graminoid Survival") +
+  scale_y_continuous(limits = c(0,1)) +
+  scale_color_manual(labels = c("dry year", "wet year"), values = c("#fc8d62","#66c2a5")) +
+  scale_fill_manual(values = c("#fc8d62","#66c2a5"), guide = FALSE) +
+  facet_wrap(~trait, scales = "free_x", strip.position =  "bottom", ncol = 1) +
+  theme_classic()+
+  theme(legend.position = "bottom", legend.title = element_blank(), legend.background = element_rect(fill="grey95",size=0.5, linetype="solid"), strip.background = element_rect(colour=NA, fill=NA), strip.placement = "outside", strip.text.x = element_text(margin = margin(0, 0, 1.5, 0)), plot.title = element_text(hjust = 0.5, size = 13, face = "bold"))
+
+## make figures for growth data
+meanSPEI<- mean(CO_grow_TLP$SPEI_s)
+sdSPEI <- sd(CO_grow_TLP$SPEI_s)
+#get 97.5 quantile of the distribution
+SPEI_97_5 <- qnorm(.975, meanSPEI, sdSPEI) 
+SPEI_2_5 <- qnorm(.025, meanSPEI, sdSPEI)
+
+spei_vals <- c(SPEI_2_5, SPEI_97_5)
+#for TLP
+TLP_vals_g <- seq(min(CO_grow_TLP$TLP_s, na.rm = TRUE), max(CO_grow_TLP$TLP_s, na.rm = TRUE), length.out = 20)
+TLP_grow_dat <- ggpredict(mSize_TLP, terms = c("TLP_s[TLP_vals]", "SPEI_s[spei_vals]"), type = "fixed", back.transform = TRUE)
+
+#for LDMC
+LDMC_vals <- seq(min(CO_grow_LDMC$LDMC_s, na.rm = TRUE), max(CO_grow_LDMC$LDMC_s, na.rm = TRUE), length.out = 20)
+LDMC_grow_dat <- ggpredict(mSize_LDMC, terms = c("LDMC_s[LDMC_vals]", "SPEI_s[spei_vals]"), type = "fixed", back.transform = TRUE)
+
+
+#make a data.frame to contain all of the values for each trait
+UniqueGrowDat <- data.frame(trait = c("scaled(Turgor Loss Point) (MPa)"), x = TLP_grow_dat$x, grow = TLP_grow_dat$predicted, CI_low = TLP_grow_dat$conf.low, CI_high = TLP_grow_dat$conf.high, SPEI = TLP_grow_dat$group)
+
+UniqueGrowDat <- rbind(UniqueGrowDat, data.frame(trait = c("scaled(Leaf Dry Matter Content) (g/g)"), x = LDMC_grow_dat$x, grow = LDMC_grow_dat$predicted, CI_low = LDMC_grow_dat$conf.low, CI_high = LDMC_grow_dat$conf.high, SPEI = LDMC_grow_dat$group))
+
+#make data for rug plot
+RugDat_grow <-  data.frame(rug = CO_grow_TLP$TLP_s, trait = "scaled(Turgor Loss Point) (MPa)")
+RugDat_grow <- rbind(RugDat_surv, data.frame(rug = CO_grow_LDMC$LDMC_s, trait = "scaled(Leaf Dry Matter Content) (g/g)"))
+
+#make a multipanel figure that shows only the graminoid survival probs for 3 traits
+UniqueSPEI_Grow <- ggplot(data = UniqueGrowDat) +
+  geom_ribbon(aes(x = x, ymin = CI_low, ymax = CI_high, fill = SPEI), alpha = 0.3) +
+  geom_line(aes(x=x, y = grow, col = SPEI))  + 
+  geom_rug(aes(x = rug), data = RugDat_grow) +
+  labs(title = "Graminoid Size") +
+  xlab(NULL) +
+  ylab("Size in year t+1") +
+  #scale_y_continuous(limits = c(0,1)) +
+  scale_color_manual(labels = c("dry year", "wet year"), values = c("#fc8d62","#66c2a5")) +
+  scale_fill_manual(values = c("#fc8d62","#66c2a5"), guide = FALSE) +
+  facet_wrap(~trait, scales = "free_x", strip.position =  "bottom", ncol = 1) +
+  theme_classic()+
+  theme(legend.position = "bottom", legend.title = element_blank(), legend.background = element_rect(fill="grey95",size=0.5, linetype="solid"), strip.background = element_rect(colour=NA, fill=NA), strip.placement = "outside", strip.text.x = element_text(margin = margin(0, 0, 1.5, 0)), plot.title = element_text(hjust = 0.5, size = 13, face = "bold"))
+
+##Figures showing response of graminoid survival and growth using uniform SPEI
+plot_grid(UniqueSPEI_Surv, UniqueSPEI_Grow, ncol = 2, align = "h", axis = "tb", rel_widths = c(.9,1))
+
+
+#### compare SPEI between species (using species-specific values) ####
+#SPEI_unique is SPEI value unique for each species
+#SPEI_s is the scaled version of this variable
+
+#SPEI_uniform is SPEI value for all species
+#SPEI_uniform_s is the scaled version of this variable
+
+## compare raw values for unique SPEI accross species
+temp <- CO_grams[,c("species", "year_t", "SPEI_unique", "SPEI_uniform")]
+gramsSPEI_unique <- aggregate(temp[,c("SPEI_unique", "SPEI_uniform")], by = list(temp$species, temp$year_t), FUN = mean) %>% 
+  rename(species = Group.1, year_t = Group.2) %>% 
+  arrange(species) %>% 
+  filter(is.na(SPEI_unique) == FALSE) %>% 
+  mutate(year_num = as.numeric(as.character(year_t)))
+
+## get photosynthetic pathway data
+CO_traits_temp <- CO_traits[,c("species", "C3_C4")]
+## merge w/ SPEI data
+gramsSPEI_unique <- left_join(gramsSPEI_unique, CO_traits_temp)
+#add additional pathway data
+gramsSPEI_unique[gramsSPEI_unique$species=="Schedonnardus paniculatus", "C3_C4"] <- "C4"
+gramsSPEI_unique[gramsSPEI_unique$species=="Muhlenbergia torreyi", "C3_C4"] <- "C4"
+gramsSPEI_unique[gramsSPEI_unique$species=="Carex spp.", "C3_C4"] <- "C3"
+gramsSPEI_unique[gramsSPEI_unique$species=="Carex filifolia", "C3_C4"] <- "C3"
+
+gramsSPEI_unique <- gramsSPEI_unique %>% 
+  mutate(C3_C4 = as.factor(C3_C4))
+
+## plot the unique SPEI values (grams only)
+# plotted by species
+ggplot(data = gramsSPEI_unique) +
+  geom_line(aes(x = year_num, y = SPEI_unique, col = C3_C4, lty = species)) +
+  geom_line(aes(x = year_num, y = SPEI_uniform)) +
+  theme_classic()
+# plotted by photosynthetic type (grams only)
+ggplot(data = gramsSPEI_unique) +
+  geom_boxplot(aes(x = year_t, y = SPEI_unique, col = C3_C4)) +
+geom_point(aes(x = year_t, y = SPEI_uniform), col= "black", shape = 8) +
+  theme_classic()
+
+## plot the uniform SPEI values (grams only)
+ggplot(data = gramsSPEI_unique) +
+  geom_line(aes(x = year_num, y = SPEI_uniform)) +
+  theme_classic()
+
+## is the variation between years greater than the variation between species in one year?
+testM <- lm(SPEI_unique ~ year_t + species, data = gramsSPEI_unique)
+anova(testM)
+#year is a more important predictor than species (is significant, while species isn't)
+
+## Do this, but with forbs 
+temp <- CO_point_all[,c("species", "year_t", "SPEI_unique", "SPEI_uniform")]
+forbsSPEI <- aggregate(temp[,c("SPEI_unique", "SPEI_uniform")], by = list(temp$species, temp$year), FUN = mean) %>% 
+  rename(species = Group.1, year_t = Group.2) %>% 
+  arrange(species) %>% 
+  filter(is.na(SPEI_unique) == FALSE) %>% 
+  mutate(year_num = as.numeric(as.character(year_t)))
+
+## merge photosynthetic pathway with SPEI data
+forbsSPEI <- left_join(forbsSPEI, CO_traits_temp)
+#add additional pathway data
+forbsSPEI[, "C3_C4"] <- "C3" #assuming all forbs are C3
+forbsSPEI <- forbsSPEI %>% 
+  mutate(C3_C4 = as.factor(C3_C4))
+
+## plot the unique SPEI values (forbs only)
+# plotted by species
+ggplot(data = forbsSPEI) +
+  geom_line(aes(x = year_num, y = SPEI_unique, col = C3_C4, lty = species)) +
+  #geom_line(aes(x = year_num, y = SPEI_uniform)) +
+  theme_classic()
+# plotted by year
+ggplot(data = forbsSPEI) +
+  geom_boxplot(aes(x = year_t, y = SPEI_unique)) +
+  geom_point(aes(x = year_t, y = SPEI_uniform), col= "red", shape = 8) +
+  theme_classic()
+
+## plot the uniform SPEI values (forbs only)
+ggplot(data = forbsSPEI) +
+  geom_line(aes(x = year_num, y = SPEI_uniform)) +
+  theme_classic()
+
+## is the variation between years greater than the variation between species in one year?
+testM_forb <- lm(SPEI_unique ~ year_t + species, data = forbsSPEI)
+anova(testM_forb)
